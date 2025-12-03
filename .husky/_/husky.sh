2a74ab52bd8f4488684a496bb66a22750ecc3e59
inyoo403 <inyoo@ucsc.edu>
2025-12-03 10:51:25 +0000

    v2

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,34 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-if [ -z "$husky_skip_init" ]; then
-  debug () {
-    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
-  }
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-  readonly hook_name="$(basename "$0")"
-  debug "starting $hook_name..."
-
-  if [ "$HUSKY" = "0" ]; then
-    debug "HUSKY env variable is set to 0, skipping hook"
-    exit 0
-  fi
-
-  if [ ! -f package.json ]; then
-    debug "package.json not found, skipping hook"
-    exit 0
-  fi
-
-  command_exists () {
-    command -v "$1" >/dev/null 2>&1
-  }
-
-  husky_ci () {
-    [ -n "$CI" ] && [ "$CI" != "false" ]
-  }
-
-  if husky_ci; then
-    debug "CI detected, skipping install"
-    exit 0
-  fi
-fi
-
-
+They WILL FAIL in v10.0.0
+"
